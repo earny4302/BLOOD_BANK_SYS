4bd2f9556c3bb9679b4c54fa24a54f9a9c26d1a5
earny4302 <73746161+earny4302@users.noreply.github.com>
2021-07-17 20:36:01 +0530

    Update blood_bank_sys.cpp

--- blood_bank_sys.cpp
+++ blood_bank_sys.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
-
+class donor
+{
+    int no;
+    string name;
+    string address;
+    long int mobile_no;
+    int day;
+    int month;
+    int year;
+    string blood_type;
+    int amount_ml;
+};
 int main()
 {
     return 0;
