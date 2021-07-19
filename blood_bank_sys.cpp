cc1452bde19887c27bb0ad2c3bf147adc72742c3
earny4302 <73746161+earny4302@users.noreply.github.com>
2021-07-19 07:28:10 +0530

    Update blood_bank_sys.cpp

--- blood_bank_sys.cpp
+++ blood_bank_sys.cpp
@@ -1,6 +1,58 @@
 #include<iostream>
 #include<conio.h>
+#include<vector>
 using namespace std;
+class recipient;
+int cal_A_pos()
+{
+    int a;
+    for(int i=0)
+}
+int cal_A_neg()
+{
+
+}
+int cal_B_pos()
+{
+
+}
+int cal_B_neg()
+{
+
+}
+int cal_AB_pos()
+{
+
+}
+int cal_AB_neg()
+{
+
+}
+int cal_O_pos()
+{
+
+}
+int cal_O_neg()
+{
+
+}
+
+
+void dashboard()
+{
+    int A_pos,A_neg,B_pos,B_neg,AB_pos,AB_neg,O_pos,O_neg;
+    A_pos=cal_A_pos();
+    A_neg=cal_A_neg();
+    B_pos=cal_B_pos();
+    B_neg=cal_B_neg();
+    AB_pos=cal_AB_pos();
+    AB_neg=cal_AB_neg();
+    O_pos=cal_O_pos();
+    O_neg=cal_O_neg();
+
+
+
+}
 class donor
 {
     int no;
@@ -13,6 +65,9 @@ class donor
     int year;
     string blood_type;
     int amount_ml;
+    public:
+    friend void dashboard();
+    void get_detail();
 };
 class recipient
 {
@@ -26,15 +81,35 @@ class recipient
     int year;
     string blood_type;
     int amount_ml;
+    public:
+    friend void dashboard();
 };
+
+int check_criteria()
+{
+
+}
 int main()
 {
-    int option;
+    int choice;
     do
     {
-        clrscr();
-        cout<<"\n"
+        system("CLS");
+        dashboard();
+        cout<<"\nHOW CAN WE HELP YOU?";
+        cin>>choice;
+        switch (choice)
+        {
+            case 1:
+            int x= check_criteria();
+            if(x==1)
+                
+            break;
         
-    }while(option!=4);
+        default:
+            break;
+        }
+        
+    }while(choice!=4);
     return 0;
 }
